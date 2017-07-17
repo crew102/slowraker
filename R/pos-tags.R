935e2687120590ddb76d5c3d78444e8b1a7c9614
Chris Baker <chriscrewbaker@gmail.com>
2017-07-17 18:31:07 -0400

    Add pos_tags data

---
+++ R/pos-tags.R
@@ -0,0 +1,20 @@
+# Taken from http://martinschweinberger.de/docs/articles/PosTagR.pdf
+
+#' @export
+pos_tags <-
+  structure(c("Coordinating conjunction", "Cardinal number", "Determiner",
+              "Existential there", "Foreign word",
+              "Preposition or subordinating conjunction",
+              "Adjective", "Adjective, comparative", "Adjective, superlative",
+              "List item marker", "Modal", "Noun, singular or mass", "Noun, plural",
+              "Proper noun, singular", "Proper noun, plural", "Predeterminer",
+              "Possessive ending", "Personal pronoun", "Possessive pronoun",
+              "Adverb", "Adverb, comparative", "Adverb, superlative", "Particle",
+              "Symbol", "to", "Interjection", "Verb, base form", "Verb, past tense",
+              "Verb, gerund or present participle", "Verb, past participle",
+              "Verb, non–3rd person singular present", "Verb, 3rd person singular present",
+              "Wh–determiner", "Wh–pronoun", "Possessive wh–pronoun", "Wh–adverb"
+  ), .Names = c("CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR",
+                "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS",
+                "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB",
+                "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"))
