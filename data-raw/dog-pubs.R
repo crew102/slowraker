d43fb01592a4dff5ea15cc3df9c61126be3a9fb8
Chris Baker <chriscrewbaker@gmail.com>
2017-07-18 19:39:59 -0400

    Add dog pubs data frame for example data

---
+++ data-raw/dog-pubs.R
@@ -0,0 +1,30 @@
+library(fulltext)
+library(dplyr)
+library(magrittr)
+library(devtools)
+
+dog_pubs <- ft_search(query = 'dog', from = 'plos', limit = 50)
+x <- ft_get(dog_pubs)
+
+raw_dog_pubs <- data.frame(
+  title = x %>%
+    chunks("title") %>%
+    unlist(),
+  abstract = x %>%
+    chunks("abstract") %>%
+    extract2("plos") %>%
+    sapply(function(x) paste(x$abstract, collapse = " ")),
+  stringsAsFactors = FALSE, row.names = NULL
+)
+
+# Section headers appear abut words in abstracts (e.g., "BackgroundHi there").
+# We'll remove cases of this so it doesn't influence keyword extraction.
+dog_pubs <-
+  raw_dog_pubs %>%
+    mutate(abstract = gsub("(Background|Methodology/Principal Findings|Author Summary|Conclusions/significance)([[:upper:]])", " \\2", abstract)) %>%
+    mutate_all(enc2utf8) %>%
+    filter(nchar(abstract) > 10) %>%
+    slice(1:30)
+
+write.csv(dog_pubs, "data-raw/dog_pubs.csv", row.names = FALSE)
+use_data(dog_pubs, internal = FALSE, overwrite = TRUE)
