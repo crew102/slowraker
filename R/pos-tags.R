b0c75fa11c757038841eb18197e025b159c8a0c6
Chris Baker <chriscrewbaker@gmail.com>
2017-07-19 18:08:18 -0400

    Convert pos_tags to df

--- R/pos-tags.R
+++ R/pos-tags.R
@@ -2,19 +2,25 @@
 
 #' @export
 pos_tags <-
-  structure(c("Coordinating conjunction", "Cardinal number", "Determiner",
+  structure(list(
+    tag = c("CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS",
+            "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS", "PRP", "PRP$",
+            "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG",
+            "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"),
+    dscrp = c("Coordinating conjunction", "Cardinal number", "Determiner",
               "Existential there", "Foreign word",
-              "Preposition or subordinating conjunction",
-              "Adjective", "Adjective, comparative", "Adjective, superlative",
-              "List item marker", "Modal", "Noun, singular or mass", "Noun, plural",
-              "Proper noun, singular", "Proper noun, plural", "Predeterminer",
-              "Possessive ending", "Personal pronoun", "Possessive pronoun",
-              "Adverb", "Adverb, comparative", "Adverb, superlative", "Particle",
-              "Symbol", "to", "Interjection", "Verb, base form", "Verb, past tense",
+              "Preposition or subordinating conjunction", "Adjective",
+              "Adjective, comparative", "Adjective, superlative",
+              "List item marker", "Modal", "Noun, singular or mass",
+              "Noun, plural", "Proper noun, singular", "Proper noun, plural",
+              "Predeterminer", "Possessive ending", "Personal pronoun",
+              "Possessive pronoun", "Adverb", "Adverb, comparative",
+              "Adverb, superlative", "Particle", "Symbol", "to", "Interjection",
+              "Verb, base form", "Verb, past tense",
               "Verb, gerund or present participle", "Verb, past participle",
-              "Verb, non-3rd person singular present", "Verb, 3rd person singular present",
-              "Wh-determiner", "Wh-pronoun", "Possessive wh-pronoun", "Wh-adverb"
-  ), .Names = c("CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR",
-                "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS",
-                "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB",
-                "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"))
+              "Verb, non-3rd person singular present",
+              "Verb, 3rd person singular present", "Wh-determiner",
+              "Wh-pronoun", "Possessive wh-pronoun", "Wh-adverb")
+    ),
+  .Names = c("tag", "dscrp"), row.names = c(NA, -36L), class = "data.frame"
+  )
