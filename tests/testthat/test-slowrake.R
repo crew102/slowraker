bc2778bfcb16fd277af07743d38cde417eb781e1
Chris Baker <chriscrewbaker@gmail.com>
2017-07-21 18:04:20 -0400

    Add passing tests for slowrake function

---
+++ tests/testthat/test-slowrake.R
@@ -0,0 +1,48 @@
+context("slowrake")
+
+data("dog_pubs")
+
+test_that("slowrake works for non-atomic, non-empty char vectors", {
+  out <- slowrake(dog_pubs$abstract[1:10])
+  expect_equal(length(out), 10)
+})
+
+test_that("slowrake returns a score when there is only one word in txt", {
+  out <- slowrake("dog")
+  expect_true(is.numeric(out[[1]]$score))
+})
+
+test_that("slowrake works for txt without alpha chars", {
+  out <- slowrake("")
+  expect_true(is.na(unlist(out)))
+})
+
+test_that("slowrake works when all txt is removed after pos filter", {
+  out <- slowrake("quickly walk")
+  expect_true(is.na(unlist(out)))
+})
+
+test_that("slowrake removes stopwords", {
+  out <- slowrake("dogs", stop_words = "dogs")
+  expect_true(is.na(unlist(out)))
+})
+
+test_that("slowrake works when no stopwords are specified", {
+  out1 <- slowrake("yourself", stop_words = NULL)
+  out2 <- slowrake("dog", stop_words = NULL)
+  expect_true(is.na(unlist(out1)) && nrow(out2[[1]]) == 1)
+})
+
+test_that("pos filtering works as expected", {
+  out1 <- slowrake("dogs are awesome")
+  out2 <- slowrake("dogs are awesome", keep_pos = NULL)
+  out3 <- slowrake("dogs found food", keep_pos = "VBD")
+  expect_true(
+    nrow(out1[[1]]) == 1 && nrow(out2[[1]]) == 2 && nrow(out3[[1]]) == 1
+  )
+})
+
+test_that("word_min_char filtering works as expected", {
+  out <- slowrake("dogs", word_min_char = 5)
+  expect_true(is.na(unlist(out)))
+})
