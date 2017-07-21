5b9f44cdb8b754999d9afe5cd38c151cdc03e38b
Chris Baker <chriscrewbaker@gmail.com>
2017-07-21 17:59:47 -0400

    Add doc for dog_pubs

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Dog publications
+#'
+#' A data frame containing the titles and abstracts of PLOS publications related to dogs. The purpose of this data frame is to provide an example of some text (the publications' abstracts) for which keywords can be extracted.)
+#'
+#' @format A data frame with 30 rows and 2 variables:
+#' \describe{
+#'   \item{title}{The publication's title}
+#'   \item{abstract}{The publication's abstract}
+#' }
+"dog_pubs"
