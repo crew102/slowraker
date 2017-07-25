87b7760103eb248ba465e3743f1dd6d7b33c373d
Chris Baker <chriscrewbaker@gmail.com>
2017-07-25 18:26:02 -0400

    Finish draft of object docs for rbind_rakelist and slowrake

--- R/rbind-rakelist.R
+++ R/rbind-rakelist.R
@@ -3,10 +3,16 @@
 #' @param rakelist A object of class \code{rakelist}, which you create by calling \code{\link{slowrake}}.
 #' @param doc_id An optional vector of document IDs, which should be the same length as \code{rakelist}. These IDs will be added to results data frame.
 #'
-#' @return
-#' @export
+#' @return A single data frame which contains the documents' keywords. The \code{doc_id} column in this data frame denotes which document a keyword was found in.
 #'
 #' @examples
+#' rakelist <- slowrake(txt = dog_pubs$abstract[1:5])
+#'
+#' # Without specifying doc_id:
+#' rbind_rakelist(rakelist = rakelist, doc_id = NULL)
+#' #...With specifying doc_id:
+#' rbind_rakelist(rakelist = rakelist, doc_id = dog_pubs$doi[1:5])
+#' @export
 rbind_rakelist <- function(rakelist, doc_id = NULL) {
   asrt("rakelist" %in% class(rakelist), "rakelist must be an object of class ",
        "'rakelist'. See examples.")
