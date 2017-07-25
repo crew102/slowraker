8dc8e0697d862c39dc2fb306e7b974738b923b1b
Chris Baker <chriscrewbaker@gmail.com>
2017-07-24 22:22:19 -0400

    Change print method so you can see structure of complete object

--- R/print.R
+++ R/print.R
@@ -1,11 +1,18 @@
 #' @export
 print.rakelist <- function(x, ...) {
-  df <- x[[1]]
   num_docs <- length(x)
+  up_ind <- ifelse(num_docs == 1, 1, 2)
 
   cat(
-    "#### A list of", as.character(num_docs), "data frames,",
-    "where each data frame contains a document's keywords. E.g.:\n\n"
+    "#### A list of", as.character(num_docs), "data frames:\n"
   )
-  print(tail(df, 3), row.names = FALSE)
+
+  utils::str(x[1:up_ind], vec.len = 1, max.level = 2, give.attr = FALSE,
+             digits.d = 1, strict.width = "cut", give.head = FALSE,
+             no.list = TRUE)
+
+  if (num_docs > 2) {
+    frames <- ifelse(num_docs == 3, "frame.", "frames.")
+    cat("#...With", as.character(num_docs - 2), "more data", frames)
+  }
 }
