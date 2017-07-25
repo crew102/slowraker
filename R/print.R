3926e7526a3a223768183287ed93e4b132aed682
Chris Baker <chriscrewbaker@gmail.com>
2017-07-25 18:24:41 -0400

    Make small tweaks to print.rakelist

--- R/print.R
+++ R/print.R
@@ -4,11 +4,11 @@ print.rakelist <- function(x, ...) {
   up_ind <- ifelse(num_docs == 1, 1, 2)
 
   cat(
-    "#### A list of", as.character(num_docs), "data frames:\n"
+    "# A rakelist containing", as.character(num_docs), "data frames:\n"
   )
 
   utils::str(x[1:up_ind], vec.len = 1, max.level = 2, give.attr = FALSE,
-             digits.d = 1, strict.width = "cut", give.head = FALSE,
+             digits.d = 2, strict.width = "cut", give.head = FALSE,
              no.list = TRUE)
 
   if (num_docs > 2) {
