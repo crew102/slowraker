b941d85fda6166237fbce3f281c2d12b850dd9fc
Chris Baker <chriscrewbaker@gmail.com>
2017-07-24 20:38:24 -0400

    Make phrase regex easier to understand

--- R/slowrate-helpers.R
+++ R/slowrate-helpers.R
@@ -1,5 +1,5 @@
 gen_split_regex <- function(stop_words) {
-  phrase_end_regex <- "[,.?():;-]|\""
+  phrase_end_regex <- '[,.?():;"-]'
   if (!is.null(stop_words)) {
     stop_words_wrds <- paste0("\\b", stop_words, "\\b")
     stop_regex <- paste0(stop_words_wrds, collapse = "|")
