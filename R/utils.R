ee4538fa8278664bdd10f1c3050a23cd47a57020
christopher crew baker <chriscrewbaker@gmail.com>
2019-02-02 00:26:56 -0500

    refactor utils

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,1 @@
-paste0_stop <- function(...) stop(paste0(...), call. = FALSE)
-
-asrt <- function (expr, ...) if (!expr) paste0_stop(...)
+asrt <- function (expr, ...) if (!expr) stop(..., call. = FALSE)
